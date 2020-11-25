510f45cb632516f3db2eb110ebb8bb26aa98517c
kickafarce <sebastien.rossignol@gmail.com>
2020-11-25 15:06:26 +0100

    other version 202
    202 characters...

--- DontPanic.py
+++ DontPanic.py
@@ -1,11 +1,12 @@
 s=lambda:input().split()
 a=s()
 t={a[3]:a[4]}
-for i in range(int(a[7])):
+n=int(a[7])
+while n:
  u,v=s()
  t[u]=v
+ n-=1
 while t:
  d,c,h=s()
- try:k=int(c)-int(t[d])
- except:k=0
+ k=int(c)-int(t[d])if d in t else 0
  print(['WAIT','BLOCK'][('R'in h)&(k>0)|('L'in h)&(k<0)])
